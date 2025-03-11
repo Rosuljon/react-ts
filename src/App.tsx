70464afeba4f5b1b34ce19c7e4a7546a54dce0c9
Rosuljon <ikramjanov97@mail.ru>
2025-03-12 06:50:17 +0900

    feat: managing state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 import NewTodo from "./components/NewTodo";
 import { Todos } from "./components/Todos";
 import Todo from "./modals/todo";
 
 function App() {
-  const onAddTodo = (text: string) => {};
-  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+  const onAddTodo = (text: string) => {
+    setTodos([...todos, new Todo(text)]);
+  };
+  const [todos, setTodos] = useState<Todo[]>([]);
   return (
     <div>
       <NewTodo onAddTodo={onAddTodo} />
